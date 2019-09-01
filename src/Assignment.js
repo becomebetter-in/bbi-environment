a917e527c51b4ce38d428f62036cb3417b8b189a
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2019-09-01 17:33:39 +0200

    fix ignore

---
+++ src/Assignment.js
@@ -0,0 +1,6 @@
+import React from "react";
+export default function(props) {
+  return (
+    <div className="card">{/*that's where the user builds his stuff*/}</div>
+  );
+}
---
+++ src/Headline.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default ({name, desciption }) => 
+<><h1><i>{name} </i>Assignment</h1>
+  <p className="desciption">
+    {desciption}
+  </p>
+</>
