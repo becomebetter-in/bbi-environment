fabf4137a6e667861d26d4de6d7daf18b6ec2024
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2019-09-06 22:20:15 +0200

    add context playground

--- src/CodeExample.js
+++ src/CodeExample.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { createContext } from "react";
+
+const NumberContext = createContext();
 
 export default function() {
-  return <div className="card">{/* here does the code go */}</div>;
+  return (
+    <div className="card">
+      <NumberContext.Provider value={42}>
+        <div>
+          <Display />
+        </div>
+      </NumberContext.Provider>
+    </div>
+  );
+}
+
+function Display() {
+  return (
+    <NumberContext.Consumer>
+      {value => <div>The answer is {value}.</div>}
+    </NumberContext.Consumer>
+  );
 }
