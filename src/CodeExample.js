e882b9bdc8e3d1ab28cf3c567fcdf0817f7be017
Robin Böhm <robin.boehm@workshops.de>
2019-09-24 11:27:11 +0200

    feat(useState) Solution

--- src/CodeExample.js
+++ src/CodeExample.js
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
 
 export default function() {
+  const [count, setCount] = useState(0);
   return (
     <div class="card">
-      {/* Your solution goes here! =) */}
+      <p>Clicked {count}x!</p>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>
+        Drecrement
+      </button>
+      <button onClick={() => setCount(0)}>
+        Reset
+      </button>
     </div>
   );
 }
